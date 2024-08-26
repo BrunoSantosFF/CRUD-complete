ef87842c3263d9291ca361d95afb2a8427e2304b
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-08-26 17:46:45 -0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-general">
+      <div className="container-main">
+        C R U D
+      </div>
+      <div className="container-commands">
+        create read update delete
+      </div>
+      
     </div>
+    
   );
 }
 
