9fc47d5c00776962aebd15b10dd94ebd3a4e49d1
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-08-28 17:05:34 -0300

    initializing functions :anger:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ import { CiRead } from "react-icons/ci";
 import { IoPencil } from "react-icons/io5";
 import { MdDelete } from "react-icons/md";
 
+import React, { useState } from 'react';
+
 function App() {
+  const [create_, setCreate] = useState();
+  const [read_, setRead] = useState();
+  const [update_, setUpdate] = useState();
+  const [delete_, setDelete] = useState();
+
+  const functionCreate = () => {
+    
+  };
+
+  const functionRead = () => {
+    
+  };
+
+  const functionUpdate = () => {
+    
+  };
+
+  const functionDelete = () => {
+    
+  };
+
   return (
     <div className="container-general">
       <div className="container-main">
@@ -16,16 +39,16 @@ function App() {
       </div>
       <div className="container-options">
         <div className="class-icon">
-          <button className="class-button"><GrAddCircle className="icons"/></button>
+          <button onClick={functionCreate} className="class-button"><GrAddCircle className="icons"/></button>
         </div>
         <div className="class-icon">
-          <button className="class-button"><CiRead className="icons"/></button>
+          <button onClick={functionRead} className="class-button"><CiRead className="icons"/></button>
         </div>
         <div className="class-icon">
-          <button className="class-button"><IoPencil className="icons"/></button>
+          <button onClick={functionUpdate} className="class-button"><IoPencil className="icons"/></button>
         </div>
         <div className="class-icon">
-          <button className="class-button"><MdDelete className="icons"/></button>
+          <button onClick={functionDelete} className="class-button"><MdDelete className="icons"/></button>
         </div>
       </div>
       
