796291c8306fce81e9995bd80d7974a5fdee7492
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-09-02 12:48:09 -0300

    add new update page

--- src/pages/ScreenUpdate.js
+++ src/pages/ScreenUpdate.js
@@ -3,55 +3,68 @@ import styles from '../styles/App.module.css';
 import stylesCreate from '../styles/ScreenCreate.module.css';
 import { fetchUsers } from '../services/useServices';
 
-function ScreenUpdate({functionSelect}) {
+import ScreenUpdateEdit from "./ScreenUpdateEdit"
+
+function ScreenUpdate({ functionSelect }) {
   const [users, setUsers] = useState([]);
   const [edit, setEdit] = useState(false);
-
 
   useEffect(() => {
     const loadUsers = async () => {
       try {
         const usersData = await fetchUsers();
         setUsers(usersData);
-
-      } 
-      catch (error) {
+      } catch (error) {
         console.error(error);
-      } 
+      }
     };
 
     loadUsers();
   }, []);
 
   const functionTest = () => {
-    setEdit(true)
-  }
+    setEdit(true);
+  };
 
   return (
-
     <div className={styles.container_general}>
-      <div className={stylesCreate.title}>
-        <h1>UPDATE</h1>
-      </div>
-      <p>Name - AGE</p>
-      <div className= {stylesCreate.container_list}>
-        {users.length > 0 ? (
-          <div>
-            {users.map(user => (
-              <div key={user.id} className={stylesCreate.container_user}>
-                <button type='button' onClick={() => functionTest()} className={stylesCreate.container_buttonEdit}>{user.name} - {user.age}</button>
-              </div>
-              
-            ))}
+      {!edit && (
+        <>
+          <div className={stylesCreate.title}>
+            <h1>UPDATE</h1>
           </div>
-        ) : (
-          <p>No users found</p>
-        )}
-      </div>
-      <div className={stylesCreate.container_button}>  
-        <button type='button' onClick={() => functionSelect('crud')}>Back</button>
-      </div>
-      
+          <p>Name - AGE</p>
+          <div className={stylesCreate.container_list}>
+            {users.length > 0 ? (
+              <div>
+                {users.map((user) => (
+                  <div key={user.id} className={stylesCreate.container_user}>
+                    <button
+                      type="button"
+                      onClick={functionTest}
+                      className={stylesCreate.container_buttonEdit}
+                    >
+                      {user.name} - {user.age}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No users found</p>
+            )}
+          </div>
+          <div className={stylesCreate.container_button}>
+            <button type="button" onClick={() => functionSelect('crud')}>
+              Back
+            </button>
+          </div>
+        </>
+      )}
+      {
+        edit && (
+          <ScreenUpdateEdit setEdit={setEdit}/>
+        )
+      }
     </div>
   );
 }
---
+++ src/pages/ScreenUpdateEdit.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/App.module.css';
+import stylesCreate from '../styles/ScreenCreate.module.css';
+import { fetchUsers } from '../services/useServices';
+
+function ScreenUpdateEdit ({setEdit}){
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const functionSubmit = () => {
+    
+  }
+
+  const functionName = (event) => {
+    setName(event.target.value);
+  }
+
+  const functionAge = (event) => {
+    setAge(event.target.value)
+  }
+
+  const functionEdit = () => {
+    setEdit(false)
+  }
+
+  return (
+    <div className={styles.container_general}>
+          
+      <h1>Insert info</h1>
+      <form onSubmit={functionSubmit} className={stylesCreate.container_form}>
+        <div className={stylesCreate.container}>
+          <label>Name: </label>
+          <input type='text' value={name} onChange={functionName} placeholder='Insert your name'></input>
+        </div>
+        <div className={stylesCreate.container}>
+          <label>Age: </label>
+          <input type='number' value={age} onChange={functionAge} placeholder='Insert your age'></input>
+        </div>
+        <div className={stylesCreate.container_button}>
+          <button type='submit' >Save</button> 
+          <button type="button" onClick={functionEdit} >
+            Back
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ScreenUpdateEdit
